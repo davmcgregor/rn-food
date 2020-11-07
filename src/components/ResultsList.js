1e05ff61b3f2afc2464302e507de59785bc19eb4
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-07 15:02:26 +0000

    Create ResultsShowScreen component

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 import ResultsDetail from './ResultsDetail';
 
-const ResultsList = ({ title, results }) => {
+const ResultsList = ({ title, results, navigation }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.titleStyle}>{title}</Text>
       <FlatList
         horizontal
+        showsHorizontalScrollIndicator={false}
         data={results}
         keyExtractor={(results) => results.id}
         renderItem={({ item }) => {
@@ -19,9 +20,14 @@ const ResultsList = ({ title, results }) => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    marginBottom: 10,
+  },
   titleStyle: {
     fontSize: 18,
     fontWeight: 'bold',
+    marginLeft: 10,
+    marginBottom: 5,
   },
 });
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -24,9 +24,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5,
-    marginTop: 10,
     marginHorizontal: 15,
     flexDirection: 'row',
+    marginVertical: 10,
   },
   inputStyle: {
     flex: 1,
