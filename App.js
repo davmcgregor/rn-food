14490c6340282074f0c83c53f4ea602f8cab8b87
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-07 17:48:36 +0000

    Make ResultsList return null if no results

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import SearchScreen from './src/screens/SearchScreen';
-import ResultsShowScreen from './src/screens/ResultShowScreen';
+import ResultsShowScreen from './src/screens/ResultsShowScreen';
 
 const navigator = createStackNavigator(
   {
